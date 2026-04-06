c5598a4d6ba28fa8c8519dd07aaa5626c707a092
Dev1 <d.developer002@gmail.com>
2026-04-06 18:32:06 +0530

    script tag in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <head>
         <JsonLd />
+     
 
         {/* Google Tag Manager */}
         <Script id="gtm-head" strategy="afterInteractive">
@@ -84,6 +85,15 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-5FCZGTXK');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w7gl8hz54u");
           `}
         </Script>
         {/* End Google Tag Manager */}
